7a4378c8d276ee77d95cc14f6fcf0e082dd99c5e
Thi Hong Hanh Bui <thihonghanhbui97@gmail.com>
2022-06-26 16:36:25 -0400

    initial commit for midterm

---
+++ .history/server/config/db_20220626155253.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "LocalURI": "mongodb://127.0.0.1/books229",
+  "RemoteURI": "mongodb+srv://HaleyBui:<password>@com229midtermcluster.hostlaa.mongodb.net/?retryWrites=true&w=majority",
+  "HostName": "LocalHost",
+  "Secret": "someSecret"
+};
